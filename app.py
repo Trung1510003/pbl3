384560ebcb955666717d0603ef20846b93264fcd
Nguyen Trung <trung1510003@gmail.com>
2024-12-29 10:37:12 +0700

    legend

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/')
-def about():
-    return render_template('login.html')
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 
 if __name__ == '__main__':
